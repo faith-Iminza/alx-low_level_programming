55f6cd9a5348d6ca1c45fe995a135ef101f6d791
faith-Iminza <faithafundi@gmail.com>
2022-09-08 02:25:25 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@ BETTY_STYLE = "betty-style"
 #include <stdio.h>
 
 /**
- *  * main - Entry point
- *   * My hello task
- *    * Return: return 1 after program execution
- *     */
+ * main - Entry point
+ * My hello task
+ * Return: return 1 after program execution
+ */
 int main(void)
 {printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
