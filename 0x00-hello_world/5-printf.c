949068ad8eb18859442d68df7590b044abda0ea5
faith-Iminza <faithafundi@gmail.com>
2022-09-08 02:07:35 -0700

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 
 /**
@@ -6,11 +5,10 @@
  *   * Return: return 0 when successful
  *    */
 int main(void)
-{
-		printf("Size of a char: %d byte(s)\n", sizeof(char));
-			printf("Size of an int: %d byte(s)\n", sizeof(int));
-				printf("Size of a long int: %d byte(s)\n", sizeof(long int));
-					printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
-						printf("size of a float: %d byte(s)\n", sizeof(float));
-							return (0);
+{printf("Size of a char: %d byte(s)\n", sizeof(char));
+	printf("Size of an int: %d byte(s)\n", sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(long long int));
+	printf("size of a float: %d byte(s)\n", sizeof(float));
+	return (0);
 }
