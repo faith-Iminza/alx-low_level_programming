d4048dab8a2e8cb6f37edfc077d96bcd81aeb11b
faith-Iminza <faithafundi@gmail.com>
2022-09-09 03:50:57 -0700

    task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ *main - entry point
+ *Return: Always 0 (success)
+ */
+
+int main(void)
+{
+char hex;
+
+for (hex = 48; hex <= 57; hex++)
+{
+putchar(hex);
+}
+
+for (hex = 97; hex <= 102; hex++)
+{
+putchar(hex);
+}
+
+putchar('\n');
+
+return (0);
+}
