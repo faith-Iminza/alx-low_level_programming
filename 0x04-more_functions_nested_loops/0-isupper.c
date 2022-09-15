6ce467f47a7368c2ad031555214080bec77f132d
faith-Iminza <faithafundi@gmail.com>
2022-09-15 02:21:45 -0700

    task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *_isupper - evaluate if letter is uppercase
+ * *@c: print int
+ * *Return: 1 if uppercase
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
