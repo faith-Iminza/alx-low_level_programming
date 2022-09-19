f3cd7a97e69834ce8af4b91c84876215d83057ff
faith-Iminza <faithafundi@gmail.com>
2022-09-19 00:16:17 -0700

    task 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,20 +1,18 @@
-#include "main.h"
-
+#include"main.h"
 /**
- *  *puts2 - prints every character of a string
- *   *@str: A pointer to an int that will be changed
- *     *Return: void which means it is correct
+ * puts2 - prints every character of a string
+ * @str: string parameter input
+ * Return: Noting
  */
 
 void puts2(char *str)
 {
-	int a;
+int i;
 
-	for (a = 0; str[a] != '\0'; a++)
-	{
-		if (a % 2 == 0)
-			_putchar (str[a]);
-	}
-
-	_putchar ('\n');
+for (i = 0; str[i] != '\0'; ++i)
+{
+	if (i % 2 == 0)
+	_putchar(str[i]);
+}
+_putchar('\n');
 }
