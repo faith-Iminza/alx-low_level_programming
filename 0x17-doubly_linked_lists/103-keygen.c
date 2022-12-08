419bd394a6ff79288ab1bf6c2276b47a9fdcd0ee
faith-Iminza <faithjacks63@gmail.com>
2022-12-08 10:27:49 -0800

    task 11

---
+++ 0x17-doubly_linked_lists/103-keygen.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * f4 - finds the biggest number
+ * @usr:username
+ * @len: length of username
+ * return: the bigggest number
+ */
+
+int f4(char *usrn,int len)
+{}
